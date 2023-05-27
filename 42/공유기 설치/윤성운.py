6752dc6fc0193b7d1811a7784c6852e383941630
윤성운 <ysu6691@gmail.com>
2023-05-27 11:12:57 +0900

    윤성운

--- 42/공유기 설치/윤성운.py
+++ 42/공유기 설치/윤성운.py
@@ -1,27 +1,37 @@
-# 각 자리에서 네 방향으로 이동하는 함수 생성
+import sys
+input = sys.stdin.readline
 
-def move(i, j, n, num):
-    # 여섯 자리 찾았으면 함수 종료
-    if n == 6:
-        # 숫자 조합 처음 찾았다면 리스트에 추가
-        if num not in num_list:
-            num_list.append(num)
-        return
+N, C = map(int, input().split())
+nums = []
+for _ in range(N):
+    nums.append(int(input()))
+nums.sort()
 
-    # 각 네 방향으로 들어가기
-    if 0 <= i < 5 and 0 <= j < 5:
-        move(i + 1, j, n + 1, num + arr[i][j])
-        move(i - 1, j, n + 1, num + arr[i][j])
-        move(i, j + 1, n + 1, num + arr[i][j])
-        move(i, j - 1, n + 1, num + arr[i][j])
+left = 1
+right = nums[-1]
+answer = -1
+while left <= right:
+    middle = (left + right) // 2
 
-arr = [input().split() for _ in range(5)]
-num_list = []
+    idx = 0
+    cnt = 1
+    next_idx = 1
 
-# 각 자리에 대해 모두 함수 실행
-for p in range(5):
-    for q in range(5):
-        move(p, q, 0, '')
+    while idx < N and next_idx < N:
+        while nums[idx] + middle > nums[next_idx]:
+            next_idx += 1
+            if next_idx == N:
+                break
+        else:
+            cnt += 1
+            idx = next_idx
+            next_idx += 1
+            if cnt == C and answer < middle:
+                answer = middle
+                break
+    else:
+        right = middle - 1
+        continue
+    left = middle + 1
 
-# 찾은 조합 개수 출력
-print(len(num_list))
+print(answer)
